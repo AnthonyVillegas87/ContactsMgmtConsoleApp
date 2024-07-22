233988178e16b50799068e658481319b3a74f7ed
Anthony Villegas <villegasanthony87@yahoo.com>
2024-07-22 16:32:20 -0500

    feat: Contact methods for single, all, and details implemented

--- PhoneBookConsole/PhoneBook.cs
+++ PhoneBookConsole/PhoneBook.cs
@@ -4,8 +4,37 @@ public class PhoneBook
 {
     private List<Contact> _contacts { get; set; }
 
+    private void DisplayContactDetail(Contact contact)
+    {
+        Console.WriteLine($"Contact: {contact.Name}, {contact.Number}");  
+    }
+
     public void AddContact(Contact contact)
     {
         _contacts.Add(contact);
+    }
+
+    public void DisplayContact(string number)
+    {
+        var contact = _contacts.FirstOrDefault(c => c.Number == number);
+        if (contact == null)
+        {
+            Console.WriteLine("Contact not found");
+        }
+        else
+        {
+            DisplayContactDetail(contact);
+        }
+        {
+            
+        }
+    }
+
+    public void DisplayAllContacts()
+    {
+        foreach (var contact in _contacts)
+        {
+            DisplayContactDetail(contact);
+        }
     }
 }
